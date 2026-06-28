6e0b693b411850b774b46a80b80e9395cb461588
davidmarsden <dmarsd@gmail.com>
2026-06-28 10:04:28 +0100

    Add shared club alias dictionary

---
+++ src/data/clubAliases.js
@@ -0,0 +1,104 @@
+export const CLUB_ALIASES = new Map([
+  // Germany
+  ["hamburg", "Hamburger SV"],
+  ["hamburger", "Hamburger SV"],
+  ["hamburger sv", "Hamburger SV"],
+  ["hamburger s v", "Hamburger SV"],
+
+  ["bayern", "Bayern Munich"],
+  ["bayern munchen", "Bayern Munich"],
+  ["bayern munich", "Bayern Munich"],
+
+  ["hertha", "Hertha Berlin"],
+  ["h berlin", "Hertha Berlin"],
+  ["hertha berlin", "Hertha Berlin"],
+
+  ["borussia monchengladbach", "Borussia Mönchengladbach"],
+  ["borussia moenchengladbach", "Borussia Mönchengladbach"],
+  ["monchengladbach", "Borussia Mönchengladbach"],
+  ["moenchengladbach", "Borussia Mönchengladbach"],
+
+  ["wolfsburg", "VfL Wolfsburg"],
+  ["vfl wolfsburg", "VfL Wolfsburg"],
+
+  // Italy
+  ["ac milan", "AC Milan"],
+  ["milan", "AC Milan"],
+
+  ["acf fiorentina", "Fiorentina"],
+  ["fiorentina", "Fiorentina"],
+
+  ["inter", "Internazionale"],
+  ["inter milan", "Internazionale"],
+  ["internazionale", "Internazionale"],
+
+  ["juventus", "Juventus"],
+  ["juventus fc", "Juventus"],
+
+  ["roma", "Roma"],
+  ["as roma", "Roma"],
+
+  ["napoli", "Napoli"],
+  ["ssc napoli", "Napoli"],
+
+  ["sampdoria", "Sampdoria"],
+  ["uc sampdoria", "Sampdoria"],
+
+  ["sassuolo", "Sassuolo"],
+  ["sassulo", "Sassuolo"],
+
+  ["udinese", "Udinese Calcio"],
+  ["udinese calcio", "Udinese Calcio"],
+
+  // England
+  ["man utd", "Manchester United"],
+  ["manchester utd", "Manchester United"],
+  ["manchester united", "Manchester United"],
+
+  ["man city", "Manchester City"],
+  ["manchester city", "Manchester City"],
+
+  ["west brom", "West Bromwich Albion"],
+  ["west bromwich albion", "West Bromwich Albion"],
+
+  // Spain
+  ["barca", "Barcelona"],
+  ["barcelona", "Barcelona"],
+
+  ["atletico", "Atlético Madrid"],
+  ["atletico madrid", "Atlético Madrid"],
+  ["atlético madrid", "Atlético Madrid"],
+
+  ["deportivo", "Deportivo La Coruña"],
+  ["deportivo la coruna", "Deportivo La Coruña"],
+  ["deportivo la Coruña", "Deportivo La Coruña"],
+
+  // Portugal
+  ["sporting", "Sporting CP"],
+  ["sporting cp", "Sporting CP"],
+  ["sporting lisbon", "Sporting CP"],
+
+  // France
+  ["psg", "Paris Saint-Germain"],
+  ["paris saint germain", "Paris Saint-Germain"],
+  ["paris saint germain fc", "Paris Saint-Germain"],
+
+  // Netherlands / Belgium / Turkey / Russia / South America / misc
+  ["feyenoord", "Feyenoord"],
+  ["galatasaray", "Galatasaray SK"],
+  ["galatasaray sk", "Galatasaray SK"],
+  ["fenerbahce", "Fenerbahçe SK"],
+  ["fenerbahçe", "Fenerbahçe SK"],
+  ["fenerbahce sk", "Fenerbahçe SK"],
+  ["fenerbahçe sk", "Fenerbahçe SK"],
+
+  ["cska moskva", "CSKA Moskva"],
+  ["cska moscow", "CSKA Moskva"],
+
+  ["zenit", "Zenit Saint Petersburg"],
+  ["zenit saint petersburg", "Zenit Saint Petersburg"],
+  ["zenit st petersburg", "Zenit Saint Petersburg"],
+
+  ["sao paulo", "São Paulo FC"],
+  ["sao paulo fc", "São Paulo FC"],
+]);
