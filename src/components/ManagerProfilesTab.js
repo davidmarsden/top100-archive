be5c44764eeec361ce2feaedd5eafcba1bcd270e
davidmarsden <dmarsd@gmail.com>
2026-06-29 07:53:40 +0100

    Add manager profiles tab wrapper

---
+++ src/components/ManagerProfilesTab.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ManagerProfiles from "../ManagerProfiles";
+
+const ManagerProfilesTab = ({ allPositionData, winnersSet }) => (
+  <ManagerProfiles allPositionData={allPositionData} winnersSet={winnersSet} />
+);
+
+export default ManagerProfilesTab;
